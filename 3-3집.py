0e8de75741d4f47cc4fe9373ce9a8eb48a4fc65d
yisj777s <111944054+yisj777s@users.noreply.github.com>
2022-09-08 21:09:42 +0900

    Add files via upload

---
+++ 3-3집.py
@@ -0,0 +1,32 @@
+a,b=9,2
+print(a**b, a%b,a//b)
+
+a=True
+print(type(a))
+
+a = (100==100)
+b = (10>100)
+print(a, b)
+
+#문자열 데이터 형식
+
+a = "파이썬 만세"
+print(a)
+print(type(a))
+
+print("작은따옴표는 ' 모양입니다.")
+print('큰따옴표는 " 모양입니다.')
+print("큰따옴표는 \" 모양입니다.")
+
+a = "이건 큰따옴표 \" 모양."
+b = '이건 작은땅모표 \'모양.'
+print(a,b)
+
+a = '파이썬 \n만세'
+print(a)
+
+a = """파이썬
+만세"""
+print(a)
+
+print('파이썬\n만세')
