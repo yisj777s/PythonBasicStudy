219dcac9814c5a9dfa4a8d1b635c76031ce078e6
yisj777s <111944054+yisj777s@users.noreply.github.com>
2022-09-29 14:15:35 +0900

    Add files via upload

---
+++ 220928.3.913.반복문.while문.for문과 while문 비교.py
@@ -0,0 +1,7 @@
+for i in range(0,3,1):
+    print("%d: 안녕하세요? for문을 공부중^^"%i)
+
+i=0
+while i < 3:
+    print("%d: 안녕하세요? while문을 공부중^^"%i)
+    i+=1
---
+++ 220928.3.914.반복문.while문.while문을 이용한 1에서 10까지의 합.py
@@ -0,0 +1,8 @@
+hap=0
+
+i=1
+while i<11:
+    hap+=i
+    i+=1
+
+print("1에서 10까지의 합: %d"%hap)
