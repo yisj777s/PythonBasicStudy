c5839f0db09a171827be4ab8d4e690dc4d4bb43f
yisj777s <111944054+yisj777s@users.noreply.github.com>
2022-11-18 18:17:55 +0900

    Update 12-01.py

--- 12장 윈도우 창이 나오는 윈도우 프로그래밍/Section 01 기본 위젯을 활용해봅시다/221116.Wed/12-01.py
+++ 12장 윈도우 창이 나오는 윈도우 프로그래밍/Section 01 기본 위젯을 활용해봅시다/221116.Wed/12-01.py
@@ -2,9 +2,4 @@ from tkinter import *
 
 window = Tk()
 
-# 이 부분에서 화면을 구성하고 처리
-
 window.mainloop()
-
-# 1행: tkinter는 파이썬에서 GUI관련 모듈을 제공해주는 표준 윈도우 라이브러리
-# 3행: Tk()는 기본이 되는 윈도우를 반환, 루트 윈도우(Root Window)또는 베이스 윈도우(Base Window)라고 함.
